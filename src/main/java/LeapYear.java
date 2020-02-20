ea266b1ea00de9b7de48eecdd9fbba658dd54a09
Tom Spencer <tom@homeflow.co.uk>
2020-02-20 16:41:01 +0000

    Add guard clause for returning false if year isNotGregorianLeapYear or !isDivisibleByFour

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,14 +1,15 @@
 public class LeapYear {
     public boolean isLeapYear(int year){
 
-        if(isDivisibleBy100(year) && !isDivisibleByFourHundred(year)){
+        if(isNotGregorianLeapYear(year) || !isDivisibleByFour(year)){
             return false;
         }
 
-        if(isDivisibleByFour(year)){
-            return true;
-        }
-        return false;
+        return true;
+    }
+
+    private boolean isNotGregorianLeapYear(int year){
+        return isDivisibleBy100(year) && !isDivisibleByFourHundred(year);
     }
 
     private boolean isDivisibleByFour(int year){
